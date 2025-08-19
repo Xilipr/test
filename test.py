7e9c495e55fc6f5a942497df256a04f5ee3f5f81
Xilipr <0vlastelin0@gmail.com>
2025-08-19 14:49:06 +0300

    4

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
 print(54)
 print(5364)
 print("Hello,world!")
-print( "мшина едет" , "rt" , end=", 80 ml" , sep="-" )
+print( "мшина едет" , "89 ml" , end="." , sep=" - " )
+
+
+
+day='19'
+month='08'
+year='2025'
+
+x=day
+y=month
+z=year
+
+print('day:', x)
+print('month:', y)
+print('eyar:', z )
